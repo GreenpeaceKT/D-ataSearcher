de111236de9c4b5522c197bcd63de8c76836bd50
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-10-07 23:17:50 +0900

    fix

--- commands/status.js
+++ commands/status.js
@@ -9,34 +9,38 @@ module.exports = {
             .setDescription('ボスの名前')
         ),
     async execute(interaction) {
+        const menu = [];
         const input = interaction.options.getString('input');
         const bossFiles = fs.readdirSync('./boss/').filter(file => file.endsWith('.json'));
-        const sortBoss = keyword => {
-            let boss = bossFiles.filter(b => b.includes(keyword))
-            if (boss.lengh > 25) {
-                boss.slice(0, 25);
-            }
-            if (boss.lengh == 0) {
-                return
-            }
-            boss.forEach(file => {
-                const json = fs.readFileSync(`./boss/${file}`);
-                const parsed = JSON.parse(json);
-                const bossName = parsed.name;
-                const bossMap = parsed.map;
-                return new StringSelectMenuOptionBuilder()
-                    .setLabel(bossName)
-                    .setDescription(bossMap)
-                    .setValue(bossName);
-            });
+        let boss = bossFiles.filter(b => b.includes(input))
+        if (boss.lengh > 25) {
+            boss.slice(0, 25);
         }
+        if (boss.lengh == 0) {
+            return
+        }
+        boss.forEach((file) => {
+
+            const json = fs.readFileSync(`./boss/${file}`);
+            const parsed = JSON.parse(json);
+            const bossName = parsed.name;
+            const bossMap = parsed.map;
+            menu.push({
+                name: bossName,
+                map: bossMap
+            });
+        });
+        const ops = menu.map((m)=>{
+            new StringSelectMenuOptionBuilder()
+                .setLabel(m.name)
+                .setDescription(m.map)
+                .setValue(m.name)
+        })
+
         const select = new StringSelectMenuBuilder()
             .setCustomId('starter')
             .setPlaceholder('ボス名を選んでください')
-            .addOptions(/*sortBoss(input)*/new StringSelectMenuOptionBuilder()
-                    .setLabel("bossName")
-                    .setDescription("bossMap")
-                    .setValue("bossName"));
+            .addOptions(ops);
 
         const row = new ActionRowBuilder()
             .addComponents(select);
